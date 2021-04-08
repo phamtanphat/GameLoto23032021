5b15959fa474beb11c757e6abedfa647716bfe2a
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-08 20:15:53 +0700

    validate form

--- app/src/main/java/com/example/gameloto23032021/MainActivity.java
+++ app/src/main/java/com/example/gameloto23032021/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     EditText mEdtSMin,mEdtSMax;
     Button mBtnRandom;
     TextView mTvResult;
+    String mTextSMin = "";
+    String mTextSMax = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +38,23 @@ public class MainActivity extends AppCompatActivity {
         mBtnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String textSMin = mEdtSMin.getText().toString();
-                String textSMax = mEdtSMax.getText().toString();
 
-
+                int sMax = Integer.parseInt(textSMax);
+                int SMin = Integer.parseInt(textSMin);
             }
         });
+    }
+    // Cách định 1 phương thức
+    // 1 : Phạm vi hoạt động
+    // 2 : Kiểu dữ liệu trả về
+    // 3 : Tên phương thức
+    private void validateForm(){
+        mTextSMin = mEdtSMin.getText().toString();
+        mTextSMax = mEdtSMax.getText().toString();
 
-
+        if (mTextSMin.isEmpty() || mTextSMax.isEmpty()){
+            Toast.makeText(MainActivity.this, "Bạn chưa nhập đủ thông tin!!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
